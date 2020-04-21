2630142307cca3f6c5035e96efa7ebe30a1f04ab
Paul <pdeyarmin@gmail.com>
2020-04-21 08:37:45 -0400

    finished bonuses

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentComponent implements OnInit {
 
+  equipBeingEdited: object = null;
+
+  equipment: object[] = [
+    {name: "Habitat dome"}, 
+    {name: "Drones"},
+    {name: "Food containers"},
+    {name: "Oxygen tanks"}
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  add(equipName: string) {
+    this.equipment.push({name: equipName});
+  }
+
+  remove(equip: object) {
+    let index = this.equipment.indexOf(equip);
+    this.equipment.splice(index, 1);
+  }
+
+  edit(equip: object) {
+    this.equipBeingEdited = equip;
+ }
+
+ save(name: string, equip: object) {
+  equip['name'] = name;
+  this.equipBeingEdited = null;
+}
 
 }
--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperimentsComponent implements OnInit {
 
+  experimentBeingEdited: object = null;
+
+  science: object[] = [
+    {name: "Mars soil sample"},
+    {name: "Plant growth in habitat"},
+    {name: "Human bone density"}
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  add(experimentName: string) {
+    this.science.push({name: experimentName});
+  }
+
+  remove(experiment: object) {
+    let index = this.science.indexOf(experiment);
+    this.science.splice(index, 1);
+  }
+
+  edit(experiment: object) {
+    this.experimentBeingEdited = experiment;
+ }
+
+ save(name: string, experiment: object) {
+  experiment['name'] = name;
+  this.experimentBeingEdited = null;
+}
 
 }
